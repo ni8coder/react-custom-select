8d2716893685e64dc10260896c71dd82d448fd7d
Awais Iqbal <m.awais.iqb@gmail.com>
2022-12-10 00:12:37 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# react-custom-select
+
+# To start:
+
+install `npm install`
+
+start `npm run dev`
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Select, SelectOption } from "./Select";
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+];
+function App() {
+  const [value, setValue] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
+  return (
+    <>
+      <Select
+        multiple
+        options={options}
+        value={value}
+        onChange={(option) => setValue(option)}
+      />
+      <br />
+      <Select
+        options={options}
+        value={value2}
+        onChange={(option) => setValue2(option)}
+      />
+    </>
+  );
+}
+
+export default App;
